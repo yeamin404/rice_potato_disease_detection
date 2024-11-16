3e55d8d2135b955d2011c0ec1f8e5cef4d378976
yeamin404 <113089601+yeamin404@users.noreply.github.com>
2024-11-16 14:44:16 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+
+#Tensorflow Model Prediction
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("new_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    confidence = round(100 * (np.max(predictions)), 2)
+    return np.argmax(predictions), confidence #return index of max element
+
+#Sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
+
+#Main Page
+if(app_mode=="Home"):
+    st.header("DISEASE RECOGNITION SYSTEM")
+    image_path = "home.jpg"
+    st.image(image_path,use_column_width=True)
+    st.markdown("""
+    Welcome to the Plant Disease Recognition System! 🌿🔍
+    
+    Our mission is to help in identifying plant diseases efficiently. Upload an image of a plant, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
+
+    ### How It Works
+    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action.
+
+    ### Why Choose Us?
+    - **Accuracy:** Our system utilizes state-of-the-art machine learning techniques for accurate disease detection.
+    - **User-Friendly:** Simple and intuitive interface for seamless user experience.
+    - **Fast and Efficient:** Receive results in seconds, allowing for quick decision-making.
+    """)
+
+#About Project
+elif(app_mode=="About"):
+    st.header("About")
+    st.markdown("""
+                #### About Dataset
+                This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
+                This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
+                A new directory containing 33 test images is created later for prediction purpose.
+                #### Neural Network Model
+                Conolutional Neural Network (CNN) of Tensorflow Library
+
+                """)
+    
+#Prediction Page
+elif(app_mode=="Disease Recognition"):
+    st.header("Disease Recognition")
+    test_image = st.file_uploader("Choose an Image:")
+    if test_image is not None:
+        st.image(test_image, caption="Uploaded image", use_column_width=False, width=500)
+ 
+        if st.button("Predict"):
+            st.balloons()
+            st.write("Our Prediction")
+        
+            result_index, confidence = model_prediction(test_image)
+            #Reading Labels
+            class_name = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',                                'Rice_Bacterial_leaf_blight','Rice_Brown_spot', 'Rice_Healthy', 'Rice_Leaf_Blast', 'Rice_Leaf_Smut']
+            st.success("Model is predicting it's a {}, with {}% confidence.".format(class_name[result_index],confidence))
+    else:
+        st.warning("Please upload an image to proceed.")
